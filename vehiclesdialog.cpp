b3948d15a9a2ebfbeb67c6312497a61bf3c8c379
Pedro <pedromanu27@gmail.com>
2020-01-02 16:48:25 -0300

    Veiculos dialog

---
+++ vehiclesdialog.cpp
@@ -0,0 +1,6 @@
+#include "vehiclesdialog.h"
+
+VehiclesDialog::VehiclesDialog(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ vehiclesdialog.h
@@ -0,0 +1,17 @@
+#ifndef VEHICLESDIALOG_H
+#define VEHICLESDIALOG_H
+
+#include <QWidget>
+
+class VehiclesDialog : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit VehiclesDialog(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // VEHICLESDIALOG_H
